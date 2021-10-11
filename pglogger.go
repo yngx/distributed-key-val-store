29174b1a906401b05404777c8d4017f2d7974727
Xao <divxyng.edu@gmail.com>
2021-10-10 22:53:35 -0500

    Add constructor

--- pglogger.go
+++ pglogger.go
@@ -3,6 +3,13 @@ import (
 	_ "github.com/lib/pq" // Anonymously import the driver package
 )
 
+type PostgresDBParams struct {
+    dbName   string
+    host     string
+    user     string
+    password string
+}
+
 type PostgresTransactionLogger struct {
 	events chan<- Event
 	errors <-chan error
@@ -21,8 +28,36 @@ func (l *PostgresTransactionLogger) Err() <-chan error {
 	return l.errors
 }
 
-func NewPostgresTransactionLogger(host, dbName, user, password string ) (TransactionLogger, error) {
-	// fill out
+func NewPostgresTransactionLogger(config PostgresDBParams) (TransactionLogger, error) {
+	connStr := fmt.Sprintf("host=%s dbname=%s user=%s password=%s",
+        config.host, config.dbName, config.user, config.password)
+
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open db: %w", err)
+    }
+
+	err = db.Ping()                 // Test the database connection
+    if err != nil {
+        return nil, fmt.Errorf("failed to open db connection: %w", err)
+    }
+
+    logger := &PostgresTransactionLogger{db: db}
+
+	// implement verify table exists
+    exists, err := logger.verifyTableExists()
+    if err != nil {
+        return nil, fmt.Errorf("failed to verify table exists: %w", err)
+    }
+
+	if !exists {
+		// implement create Table
+        if err = logger.createTable(); err != nil {
+            return nil, fmt.Errorf("failed to create table: %w", err)
+        }
+    }
+
+    return logger, nil
 }
 
 func (l *PostgresTransactionLogger) Run() {
