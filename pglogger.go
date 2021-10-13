7fa26df1a79019a8e4f41b818c0bb4d91791b94e
Xao <divxyng.edu@gmail.com>
2021-10-12 22:49:42 -0500

    Added run, readEvents, createTable, and verifyTable functions

--- pglogger.go
+++ pglogger.go
@@ -61,9 +61,106 @@ func NewPostgresTransactionLogger(config PostgresDBParams) (TransactionLogger, e
 }
 
 func (l *PostgresTransactionLogger) Run() {
-	// fill out
+	events := make(chan Event, 16)              // Make an events channel
+    l.events = events
+
+    errors := make(chan error, 1)               // Make an errors channel
+    l.errors = errors
+
+    go func() {                                 // The INSERT query
+        query := `INSERT INTO transactions
+            (event_type, key, value)
+            VALUES ($1, $2, $3)`
+
+        for e := range events {                 // Retrieve the next Event
+
+            _, err := l.db.Exec(                // Execute the INSERT query
+                query,
+                e.EventType, e.Key, e.Value)
+
+            if err != nil {
+                errors <- err
+            }
+        }
+    }()
 }
 
 func (l *PostgresTransactionLogger) ReadEvents() (<-chan Event, <-chan Error) {
-	// fill out
+	outEvent := make(chan Event)                // An unbuffered events channel
+    outError := make(chan error, 1)             // A buffered errors channel
+
+    go func() {
+        defer close(outEvent)                   // Close the channels when the
+        defer close(outError)                   // goroutine ends
+
+        query := `SELECT sequence, event_type, key, value FROM transactions
+                  ORDER BY sequence`
+
+        rows, err := db.Query(query)            // Run query; get result set
+        if err != nil {
+            outError <- fmt.Errorf("sql query error: %w", err)
+            return
+        }
+
+        defer rows.Close()                      // This is important!
+
+        e := Event{}                            // Create an empty Event
+
+        for rows.Next() {                       // Iterate over the rows
+
+            err = rows.Scan(                    // Read the values from the
+                &e.Sequence, &e.EventType,      // row into the Event.
+                &e.Key, &e.Value)
+
+            if err != nil {
+                outError <- fmt.Errorf("error reading row: %w", err)
+                return
+            }
+
+            outEvent <- e                       // Send e to the channel
+        }
+
+        err = rows.Err()
+        if err != nil {
+            outError <- fmt.Errorf("transaction log read failure: %w", err)
+        }
+    }()
+	
+	return outEvent, outError
+}
+
+func (l *PostgresTransactionLogger) verifyTableExists() (bool, error) {
+	const table = "transactions"
+
+	var result string
+
+	rows, err := l.db.Query(fmt.Sprintf("SELECT to_regclass('public.%s');", table))
+	defer rows.Close()
+	if err != nil {
+		return false, err
+	}
+
+	for rows.Next() && result != table {
+		rows.Scan(&result)
+	}
+
+	return result == table, rows.Err()
+}
+
+func (l *PostgresTransactionLogger) createTable() error {
+	var err error
+
+	createQuery := `CREATE TABLE transactions (
+		sequence      BIGSERIAL PRIMARY KEY,
+		event_type    SMALLINT,
+		key 		  TEXT,
+		value         TEXT
+	  );`
+
+	_, err = l.db.Exec(createQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
